8ea1cbc836cab50dd0b4f6a9461d8bbb92606f13
tnath <tnath@vmware.com>
2016-09-06 19:56:56 +0530

    Added Sensor details

--- app/src/main/java/org/tamal/deviceinformation/SensorActivity.java
+++ app/src/main/java/org/tamal/deviceinformation/SensorActivity.java
@@ -3,37 +3,65 @@ package org.tamal.deviceinformation;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SensorActivity extends BaseActivity {
 
-    private SensorAdapter sensorAdapter;
-    private SensorManager sensorManager;
+    private static Map<Integer, String> sensorTypeMap = new TreeMap<>();
+    static {
+        for (Field field : Sensor.class.getDeclaredFields()) {
+            try {
+                if (int.class == field.getType()
+                        && field.getName().startsWith("TYPE_")) {
+                    sensorTypeMap.put(field.getInt(null), field.getName().substring(5));
+                }
+            } catch (IllegalAccessException e) {
+                //
+            }
+        }
+    }
+
     private List<Sensor> sensors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_sensor);
         super.onCreate(savedInstanceState);
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensors = new ArrayList<>(sensorManager.getSensorList(Sensor.TYPE_ALL));
+        Collections.sort(sensors, new Comparator<Sensor>() {
+            @Override
+            public int compare(Sensor s1, Sensor s2) {
+                String t1 = sensorTypeMap.get(s1.getType());
+                String t2 = sensorTypeMap.get(s2.getType());
+                if (t1 == null) {
+                    return 1;
+                }
+                if (t2 == null) {
+                    return -1;
+                }
+                return t1.compareTo(t2);
+            }
+        });
         RecyclerView sensorView = (RecyclerView) findViewById(R.id.sensor_view);
         sensorView.setHasFixedSize(true);
         sensorView.setLayoutManager(new LinearLayoutManager(this));
-        sensorAdapter = new SensorAdapter();
+        SensorAdapter sensorAdapter = new SensorAdapter();
         sensorView.setAdapter(sensorAdapter);
     }
 
@@ -50,28 +78,12 @@ public class SensorActivity extends BaseActivity {
         public void onBindViewHolder(SensorHolder holder, int position) {
             Sensor sensor = sensors.get(position);
             holder.sensorName.setText(sensor.getName());
-            switch (sensor.getType()) {
-                case Sensor.TYPE_ACCELEROMETER:
-                    holder.sensorType.setImageResource(R.drawable.ic_accelerometer);
-                    break;
-                case Sensor.TYPE_MAGNETIC_FIELD:
-                case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
-                    holder.sensorType.setImageResource(R.drawable.ic_magnetometer);
-                    break;
-                case Sensor.TYPE_GYROSCOPE:
-                case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
-                    holder.sensorType.setImageResource(R.drawable.ic_gyroscope);
-                    break;
-                case Sensor.TYPE_PROXIMITY:
-                    holder.sensorType.setImageResource(R.drawable.ic_proximity);
-                    break;
-                case Sensor.TYPE_LIGHT:
-                    holder.sensorType.setImageResource(R.drawable.ic_light);
-                    break;
-                case Sensor.TYPE_GRAVITY:
-                    holder.sensorType.setImageResource(R.drawable.ic_gravity);
-                    break;
+            int typeId = sensor.getType();
+            String type = sensorTypeMap.get(typeId);
+            if (type == null) {
+                type = getString(R.string.sensor_type_unknown);
             }
+            holder.sensorType.setText(type);
         }
 
         @Override
@@ -83,17 +95,18 @@ public class SensorActivity extends BaseActivity {
     private static class SensorHolder extends RecyclerView.ViewHolder {
 
         private TextView sensorName;
-        private ImageView sensorType;
+        private TextView sensorType;
 
         public SensorHolder(View itemView) {
             super(itemView);
             sensorName = (TextView) itemView.findViewById(R.id.sensorName);
-            sensorType = (ImageView) itemView.findViewById(R.id.sensorType);
+            sensorType = (TextView) itemView.findViewById(R.id.sensorType);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(view.getContext(), SensorDetailActivity.class);
-                    intent.putExtra("name", sensorName.getText());
+                    intent.putExtra("sensorName", sensorName.getText());
+                    intent.putExtra("sensorType", sensorType.getText());
                     view.getContext().startActivity(intent);
                 }
             });
--- app/src/main/java/org/tamal/deviceinformation/SensorDetailActivity.java
+++ app/src/main/java/org/tamal/deviceinformation/SensorDetailActivity.java
@@ -1,17 +1,145 @@
 package org.tamal.deviceinformation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class SensorDetailActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class SensorDetailActivity extends BaseActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TextView accuracyView;
+    private TextView timestampView;
+    private TextView valuesView;
+    private long timestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_detail);
-        String sensorName = getIntent().getStringExtra("name");
-        TextView sensorView = (TextView) findViewById(R.id.sensorName);
-        sensorView.setText(sensorName);
+        accuracyView = (TextView) findViewById(R.id.accuracy);
+        timestampView = (TextView) findViewById(R.id.timestamp);
+        valuesView = (TextView) findViewById(R.id.values);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        String sensorName = getIntent().getStringExtra("sensorName");
+        String sensorType = getIntent().getStringExtra("sensorType");
+        for (Sensor sensorItem : sensorList) {
+            if (sensorItem.getName().equals(sensorName)) {
+                sensor = sensorItem;
+                break;
+            }
+        }
+        TextView view;
+        view = (TextView) findViewById(R.id.id);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            view.setText(sensor.getId());
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.id_label).setVisibility(View.GONE);
+        }
+        view = (TextView) findViewById(R.id.name);
+        view.setText(sensorName);
+        view = (TextView) findViewById(R.id.type);
+        view.setText(sensorType);
+        view = (TextView) findViewById(R.id.type_id);
+        view.setText(String.valueOf(sensor.getType()));
+        view = (TextView) findViewById(R.id.vendor);
+        view.setText(sensor.getVendor());
+        view = (TextView) findViewById(R.id.version);
+        view.setText(String.valueOf(sensor.getVersion()));
+        view = (TextView) findViewById(R.id.type_internal);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+            view.setText(sensor.getStringType());
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.type_internal_label).setVisibility(View.GONE);
+        }
+        view = (TextView) findViewById(R.id.power);
+        view.setText(getString(R.string.sensor_power_unit, sensor.getPower()));
+        view = (TextView) findViewById(R.id.min_delay);
+        view.setText(getString(R.string.sensor_delay_unit, sensor.getMinDelay()));
+        view = (TextView) findViewById(R.id.max_delay);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            view.setText(getString(R.string.sensor_delay_unit, sensor.getMaxDelay()));
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.max_delay_label).setVisibility(View.GONE);
+        }
+        view = (TextView) findViewById(R.id.resolution);
+        view.setText(sensor.getResolution() + " unit");
+        view = (TextView) findViewById(R.id.max_range);
+        view.setText(sensor.getMaximumRange() + " unit");
+        view = (TextView) findViewById(R.id.reserved_event);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            view.setText(getString(R.string.sensor_event_unit, sensor.getFifoReservedEventCount()));
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.reserved_event_label).setVisibility(View.GONE);
+        }
+        view = (TextView) findViewById(R.id.max_event);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            view.setText(getString(R.string.sensor_event_unit, sensor.getFifoMaxEventCount()));
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.max_event_label).setVisibility(View.GONE);
+        }
+        view = (TextView) findViewById(R.id.dynamic);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            view.setText(String.valueOf(sensor.isDynamicSensor()));
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.dynamic_label).setVisibility(View.GONE);
+        }
+        view = (TextView) findViewById(R.id.wake_up);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            view.setText(String.valueOf(sensor.isWakeUpSensor()));
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.wake_up_label).setVisibility(View.GONE);
+        }
+        view = (TextView) findViewById(R.id.additional_info);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            view.setText(String.valueOf(sensor.isAdditionalInfoSupported()));
+        } else {
+            view.setVisibility(View.GONE);
+            findViewById(R.id.additional_info_label).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, sensor);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        accuracyView.setText(String.valueOf(sensorEvent.accuracy));
+        long delay = (sensorEvent.timestamp - timestamp) / 1_000_000;
+        timestampView.setText(getString(R.string.sensor_delay_unit, delay));
+        timestamp = sensorEvent.timestamp;
+        valuesView.setText(Arrays.toString(sensorEvent.values));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        accuracyView.setText(String.valueOf(accuracy));
     }
 }
